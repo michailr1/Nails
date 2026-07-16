067a9cadf95c82e8b5e71f7f105fbd1c5553b349
michailr1 <76062633+michailr1@users.noreply.github.com>
2026-07-16 23:22:40 +0300

    feat: register restricted feedback tool

--- hermes/plugins/nails_scheduling/__init__.py
+++ hermes/plugins/nails_scheduling/__init__.py
@@ -1,4 +1,5 @@
-from .schemas import NAILS_SCHEDULING
+from .feedback import save_feedback
+from .schemas import NAILS_SCHEDULING, SAVE_FEEDBACK
 from .tools import nails_scheduling
 
 
@@ -8,4 +9,10 @@ def register(ctx):
         toolset="nails_scheduling",
         schema=NAILS_SCHEDULING,
         handler=nails_scheduling,
+    )
+    ctx.register_tool(
+        name="save_feedback",
+        toolset="nails_scheduling",
+        schema=SAVE_FEEDBACK,
+        handler=save_feedback,
     )
