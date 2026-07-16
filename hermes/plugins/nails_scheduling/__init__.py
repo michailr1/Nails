fa4569e8aab5cd56753cca695dbebae241ff52f9
michailr1 <76062633+michailr1@users.noreply.github.com>
2026-07-17 01:22:52 +0300

    fix: apply profile-local shutdown policy

--- hermes/plugins/nails_scheduling/__init__.py
+++ hermes/plugins/nails_scheduling/__init__.py
@@ -1,10 +1,12 @@
 from .feedback_schema import SAVE_FEEDBACK
 from .feedback_tool import save_feedback
 from .schemas import NAILS_SCHEDULING
+from .shutdown_policy import suppress_shutdown_notifications_for_nails
 from .tools import nails_scheduling
 
 
 def register(ctx):
+    suppress_shutdown_notifications_for_nails()
     ctx.register_tool(
         name="nails_scheduling",
         toolset="nails_scheduling",
